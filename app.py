a6da4dd165ae519b17bc80d3e4cf23d3e8f2005e
CapersLever <capsers.seo@gmail.com>
2026-04-13 19:49:17 -0400

    added route for ms domain

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/ms22902107.txt")
+def test():
+    return render_template("ms22902107.txt")
+
 if __name__ == "__main__":
     app.run(debug=False)
