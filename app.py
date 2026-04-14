42269a47124f4b15adb89f203c7005ac68722fee
CapersLever <capsers.seo@gmail.com>
2026-04-13 20:08:01 -0400

    removed ms verification

--- app.py
+++ app.py
@@ -8,9 +8,5 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/ms22902107.txt")
-def test():
-    return render_template("ms22902107.txt")
-
 if __name__ == "__main__":
     app.run(debug=False)
